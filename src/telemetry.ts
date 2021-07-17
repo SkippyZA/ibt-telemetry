f4ebc6410bcecd6942f7b735461adc656bb80ef1
Steven Inskip <inskips@gmail.com>
2021-07-17 19:06:48 +0200

    Fix circular dependency

--- src/telemetry.ts
+++ src/telemetry.ts
@@ -1,11 +1,51 @@
-import { readSync } from 'fs'
+import { readSync, open } from 'fs'
 import { safeLoad as safeLoadYaml } from 'js-yaml'
+import { Observable } from 'rxjs'
+import { range } from 'ramda'
+
+import { readFileToBuffer } from './utils/read-file-to-buffer'
+
+import { SIZE_IN_BYTES as HEADER_SIZE_IN_BYTES, TelemetryHeader } from './headers/telemetry-header'
+import { SIZE_IN_BYTES as DISK_SUB_HEADER_SIZE_IN_BYTES, DiskSubHeader } from './headers/disk-sub-header'
+import { SIZE_IN_BYTES as VAR_HEADER_SIZE_IN_BYTES, VarHeader } from './headers/var-header'
 
 import { TelemetrySample } from './telemetry-sample'
-import { telemetryFileLoader } from './utils/telemetry-file-loader'
-import { TelemetryHeader } from './headers/telemetry-header'
-import { DiskSubHeader } from './headers/disk-sub-header'
-import { VarHeader } from './headers/var-header'
+
+// Open the data file and return it's file descriptor as a promise
+const openDataFile = (dataFile: string): Promise<number> => new Promise((resolve, reject) => {
+  open(dataFile, 'r', (err, fd) => {
+    err ? reject(err) : resolve(fd)
+  })
+})
+
+// Return the Telemetry header from the supplied file descriptor
+const telemetryHeaderFromFileDescriptor = async (fd: number): Promise<TelemetryHeader> =>
+  readFileToBuffer(fd, 0, HEADER_SIZE_IN_BYTES)
+    .then(TelemetryHeader.fromBuffer)
+
+// Disk sub header telemetry
+const diskSubHeaderFromFileDescriptor = async (fd: number): Promise<DiskSubHeader> =>
+  readFileToBuffer(fd, DISK_SUB_HEADER_SIZE_IN_BYTES, HEADER_SIZE_IN_BYTES)
+    .then(DiskSubHeader.fromBuffer)
+
+const sessionInfoStringFromFileDescriptor = async (fd: number, telemetryHeader: TelemetryHeader): Promise<string> =>
+  readFileToBuffer(fd, telemetryHeader.sessionInfoOffset, telemetryHeader.sessionInfoLength)
+    .then(x => x.toString('ascii'))
+
+const varHeadersFromFileDescriptor = async (fd: number, telemetryHeader: TelemetryHeader): Promise<VarHeader[]> => {
+  const numberOfVariables = telemetryHeader.numVars
+  const startPosition = telemetryHeader.varHeaderOffset
+  const fullBufferSize = numberOfVariables * VAR_HEADER_SIZE_IN_BYTES
+
+  return readFileToBuffer(fd, startPosition, fullBufferSize)
+    .then(buffer => {
+      return range(0, numberOfVariables).map(count => {
+        const start = count * VAR_HEADER_SIZE_IN_BYTES
+        const end = start + VAR_HEADER_SIZE_IN_BYTES
+        return VarHeader.fromBuffer(buffer.slice(start, end))
+      })
+    })
+}
 
 /**
  * iRacing Telemetry
@@ -32,8 +72,22 @@ export class Telemetry {
    * @param file path to *.ibt file
    * @return Telemetry instance of telemetry
    */
-  static fromFile (file: string): Promise<Telemetry> {
-    return telemetryFileLoader(file)
+  static async fromFile (file: string): Promise<Telemetry> {
+    const fd = await openDataFile(file)
+
+    const resolvedHeaders = await Promise.all([
+      telemetryHeaderFromFileDescriptor(fd),
+      diskSubHeaderFromFileDescriptor(fd)
+    ])
+    const telemetryHeader = resolvedHeaders[0] as TelemetryHeader
+    const diskSubHeader = resolvedHeaders[1] as DiskSubHeader
+
+    const [ sessionInfo, varHeaders ]: [ string, VarHeader[] ] = await Promise.all([
+      sessionInfoStringFromFileDescriptor(fd, telemetryHeader),
+      varHeadersFromFileDescriptor(fd, telemetryHeader)
+    ])
+
+    return new Telemetry(telemetryHeader, diskSubHeader, sessionInfo, varHeaders, fd)
   }
 
   /**
@@ -52,24 +106,28 @@ export class Telemetry {
   }
 
   /**
-   * Telemetry samples generator.
+   * Returns a stream of TelemetrySample objects
    */
-  * samples () {
-    let hasSample = true
-    let count = 0
-    const length = this.telemetryHeader.bufLen
+  sampleStream (): Observable<TelemetrySample> {
+    return new Observable(subscriber => {
+      let hasSample = true
+      let count = 0
+      const length = this.telemetryHeader.bufLen
 
-    const buffer = Buffer.alloc(length)
+      const buffer = Buffer.alloc(length)
 
-    while (hasSample) {
-      const start = this.telemetryHeader.bufOffset + (count++ * length)
-      const bytesRead = readSync(this.fd, buffer, 0, length, start)
+      while (hasSample) {
+        const start = this.telemetryHeader.bufOffset + (count++ * length)
+        const bytesRead = readSync(this.fd, buffer, 0, length, start)
 
-      if (bytesRead !== length) {
-        hasSample = false
-      } else {
-        yield new TelemetrySample(buffer, this.varHeaders)
+        if (bytesRead !== length) {
+          hasSample = false
+        } else {
+          subscriber.next(new TelemetrySample(buffer, this.varHeaders))
+        }
       }
-    }
+
+      subscriber.complete()
+    })
   }
 }
